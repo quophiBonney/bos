965faa6828730eff54018745944a8fa24e03240f
bonney <quophiclef@gmail.com>
2023-05-12 17:17:54 -0700

    editor

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Quote from "./Quote";
 import HomeCSS from "./Home.module.css";
 function Home() {
   return (
     <div className={HomeCSS.mainContainer}>
       <Header />
+      <Quote />
     </div>
   );
 }
---
+++ src/home/Quote.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+import {Container, Row, Col} from "react-bootstrap";
+import {EditorState} from "draft-js";
+import {Editor} from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+function Quote() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const handleChange = newState => {
+    setEditorState(newState);
+  };
+
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col sm={12} md={12} lg={12}>
+          <Editor
+            editorState={editorState}
+            onEditorStateChange={handleChange}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Quote;
